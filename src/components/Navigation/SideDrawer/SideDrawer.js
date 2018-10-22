92e0d0d2e7e436097ae3462b41afe3d75eb24973
Güneş Eser <spielersun@gmail.com>
2018-10-22 22:50:44 +0300

    try hdr pls

---
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import classes from './SideDrawer.css';
+import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import Auxiliary from '../../../hoc/Auxiliary/Auxiliary';
+
+const sideDrawer = (props) => {
+    let attachedClasses = [classes.SideDrawer, classes.Close];
+
+    if (props.open){
+        attachedClasses = [classes.SideDrawer, classes.Open];
+    }
+
+    return(
+        <Auxiliary>
+            <Backdrop clicked={props.closed} show={props.open} />
+            <div className={attachedClasses.join(' ')}>
+                <div className={classes.Logo}>
+                    <Logo />
+                </div>
+                <nav>
+                    <NavigationItems />
+                </nav>
+            </div>
+        </Auxiliary>
+    );
+};
+
+export default sideDrawer;
+
